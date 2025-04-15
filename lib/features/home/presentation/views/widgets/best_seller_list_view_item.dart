5bd4ddb3d73b038fe0230470da42c43b50fda01d
Moamen771 <memoaser86@gmail.com>
2025-04-16 00:57:51 +0200

    BestSellerItem done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:booklyapp/core/utils/styles.dart';
+import 'package:booklyapp/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 import '../../../../../core/utils/assets_data.dart';
 
@@ -28,33 +29,37 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
             SizedBox(width: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 3,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .50,
-                  child: Text(
-                    'Harry Potter And The Goblet Of Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyleTwo20,
-                  ),
-                ),
-                Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle14,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '19,99 L.E',
-                      style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 3,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .50,
+                    child: Text(
+                      'Harry Potter And The Goblet Of Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyleTwo20,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '19,99 L.E',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      BookRating(),
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
