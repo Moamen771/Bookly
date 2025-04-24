3096b8a0c552e461890e24ba3ccb9ac41bf56652
Moamen771 <memoaser86@gmail.com>
2025-04-24 22:35:42 +0200

    create custom book details app bar

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,6 +1,8 @@
+import 'package:booklyapp/core/utils/app_router.dart';
 import 'package:booklyapp/core/utils/styles.dart';
 import 'package:booklyapp/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../../../../core/utils/assets_data.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -10,59 +12,64 @@ class BestSellerListViewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: SizedBox(
-        height: 130,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      AssetsData.testImage,
+      child: GestureDetector(
+        onTap: () {
+          GoRouter.of(context).push(AppRouter.kBookDetailsView);
+        },
+        child: SizedBox(
+          height: 130,
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 2.5 / 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        AssetsData.testImage,
+                      ),
+                      fit: BoxFit.fill,
                     ),
-                    fit: BoxFit.fill,
                   ),
                 ),
               ),
-            ),
-            SizedBox(width: 30),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 3,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .50,
-                    child: Text(
-                      'Harry Potter And The Goblet Of Fire',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Styles.textStyleTwo20,
-                    ),
-                  ),
-                  Text(
-                    'J.K. Rowling',
-                    style: Styles.textStyle14,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '19,99 L.E',
-                        style: Styles.textStyle20.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
+              SizedBox(width: 30),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 3,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .50,
+                      child: Text(
+                        'Harry Potter And The Goblet Of Fire',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Styles.textStyleTwo20,
                       ),
-                      BookRating(),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
+                    ),
+                    Text(
+                      'J.K. Rowling',
+                      style: Styles.textStyle14,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '19,99 L.E',
+                          style: Styles.textStyle20.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        BookRating(),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
